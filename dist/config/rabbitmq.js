f04fafdc6e936bdb2934c69765b1de7f3bba5e05
Nhất Lâm Lưu <nhatlam10091997@gmail.com>
2020-12-09 17:39:10 +0700

    reset gitignore

--- dist/config/rabbitmq.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.rabbitmqConfig = void 0;
-const rabbitmqConfig = {
-    url: 'amqp://localhost',
-    nameQueue: 'insert-data-elasticseach'
-};
-exports.rabbitmqConfig = rabbitmqConfig;
--- dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const start_server_1 = require("./start-server");
-start_server_1.start().catch(err => {
-    console.log(`======= fail with message: ${err} =======`);
-});
